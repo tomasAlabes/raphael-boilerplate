cac8abeeeef21863b2581f6710a5bfb9c3d668d7
Tomas Alabes <tomas.alabes@gmail.com>
2013-05-11 15:07:58 -0300

    More documentation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 require([ "./raphael/raphael" ], function (Raphael) {
     var container = document.getElementById("container");
     var paper = Raphael(container, 600, 600);
+    // Check http://raphaeljs.com/reference.html#Raphael for other ways of creating the paper
 });
